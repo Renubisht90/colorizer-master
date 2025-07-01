355e7deecfcaca0113373fc270ed3776e082ae3a
Renu <brenu0939@gmail.com>
2025-07-01 11:01:57 +0530

    colorizer-master

---
+++ const rawMaterials = {.js
@@ -0,0 +1,22 @@
+const rawMaterials = {
+    wood: {
+        quantity: 1000,  // in cubic feet
+        pricePerUnit: 50 // dollars per cubic foot
+    },
+    steel: {
+        quantity: 500,   // in kg
+        pricePerUnit: 75 // dollars per kg
+    },
+    copper: {
+        quantity: 200,   // in kg
+        pricePerUnit: 120 // dollars per kg
+    },
+    aluminum: {
+        quantity: 300,   // in kg
+        pricePerUnit: 45  // dollars per kg
+    },
+    plastic: {
+        quantity: 800,   // in kg
+        pricePerUnit: 25  // dollars per kg
+    }
+};
